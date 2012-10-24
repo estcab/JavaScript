7eac3c4e18ba61cd278feda6358711145cc1a214
estcab <estcab@outlook.com>
2012-10-24 09:41:36 +0200

    Hola Mundo en Javascript

---
+++ Basic/01.js
@@ -0,0 +1,10 @@
+﻿// Cuando la ventana se cargue ejecutamos la funcion 'Saludo'
+window.onload = Saludo;
+
+/*
+ * Recuperamos el elemento con id = msg y cambiamos su HTML interno
+ */
+function Saludo() {
+    document.getElementById("msg").innerHTML = 
+        "Hola Mundo desde un archivo Javascript externo";
+}
